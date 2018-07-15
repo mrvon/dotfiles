1069028ed4bf342d207309c8f3b34a59dab5f025
dennis <mrvon@qq.com>
2018-07-15 16:26:45 +0800

    svn

---
+++ svn/diffwrap.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+import os
+
+# Configure your favorite diff program here.
+DIFF = "/usr/local/bin/nvim"
+
+# Subversion provides the paths we need as the last two parameters.
+LEFT  = sys.argv[-2]
+RIGHT = sys.argv[-1]
+
+# Call the diff command (change the following line to make sense for
+# your diff program).
+cmd = [DIFF, "-d", LEFT, RIGHT]
+os.execv(cmd[0], cmd)
+
+# Return an errorcode of 0 if no differences were detected, 1 if some were.
+# Any other errorcode will be treated as fatal.
