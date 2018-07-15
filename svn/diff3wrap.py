ca086572efb170653ae7e2605447d75485c51e88
dennis <mrvon@qq.com>
2018-07-15 16:26:45 +0800

    svn

---
+++ svn/diff3wrap.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+import os
+
+# Configure your favorite three-way diff program here.
+DIFF3 = "/usr/local/bin/nvim"
+
+# Subversion provides the paths we need as the last three parameters.
+MINE  = sys.argv[-3]
+OLDER = sys.argv[-2]
+YOURS = sys.argv[-1]
+
+# Call the three-way diff command (change the following line to make
+# sense for your three-way diff program).
+cmd = [DIFF3, "-d", OLDER, MINE, YOURS]
+os.execv(cmd[0], cmd)
+
+# After performing the merge, this script needs to print the contents
+# of the merged file to stdout.  Do that in whatever way you see fit.
+# Return an errorcode of 0 on successful merge, 1 if unresolved conflicts
+# remain in the result.  Any other errorcode will be treated as fatal.
---
+++ svn/mergewrap.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+import os
+
+# Configure your favorite merge program here.
+MERGE = "/usr/local/bin/nvim"
+
+# Get the paths provided by Subversion.
+BASE   = sys.argv[1]
+THEIRS = sys.argv[2]
+MINE   = sys.argv[3]
+MERGED = sys.argv[4]
+WCPATH = sys.argv[5]
+
+# Call the merge command (change the following line to make sense for
+# your merge program).
+cmd = [MERGE, "-d", BASE, MINE, THEIRS, MERGED, "-c", "$wincmd w", "-c", "wincmd J"]
+os.execv(cmd[0], cmd)
+
+# Return an errorcode of 0 if the conflict was resolved; 1 otherwise.
+# Any other errorcode will be treated as fatal.
