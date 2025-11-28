152f5c97ede83a88ccc55323379d84b1aaaa9f29
dennis <mrvon@qq.com>
2025-11-28 14:22:49 +0800

    wezterm

--- _wezterm.lua
+++ _wezterm.lua
@@ -5,7 +5,10 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 config.font_size = 14
-config.font = wezterm.font("Monaco Nerd Font", {weight = "Regular", italic = false})
+config.font = wezterm.font_with_fallback({
+    {family = "Monaco Nerd Font", weight = "Regular", italic = false},
+    {family = "Noto Sans Mono CJK SC", weight = "Regular", italic = false},
+})
 -- config.window_decorations = "NONE"
 config.hide_tab_bar_if_only_one_tab = true
 config.color_scheme = "Arthur"
@@ -16,5 +19,7 @@ config.keys = {
         action = wezterm.action.ToggleFullScreen,
     },
 }
+config.window_padding = {left = 0, right = 0, top = 0, bottom = 0}
+config.front_end = "OpenGL"
 
 return config
